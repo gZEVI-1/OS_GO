910083b8a8fc7b3ca9b7bb405137f85c8b8d6062
gZEVI-1 <sergorbachev27@gmail.com>
2026-02-22 12:47:09 +0400

    удалил лишнее

--- core/build/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="go_engine",
-    version="0.1",
-    packages=[],
-    package_data={'': ['go_engine*.pyd']},
-)
--- core/build/test.py
+++
@@ -1,7 +0,0 @@
-try:
-    import go_engine
-    print("✅ go_engine работает!")
-    print("Доступные функции:", dir(go_engine))
-except ImportError as e:
-    print(f"❌ Ошибка импорта: {e}")
-# 
