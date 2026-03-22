7d1dfe4c8be3712f36eb4d7ca38196f557735304
polnipish <polina.kotilev@yandex.ru>
2026-03-22 18:02:33 +0400

    Update main.py

--- interface/Go_app/main.py
+++ interface/Go_app/main.py
@@ -1,13 +1,12 @@
 import sys
 from PySide6.QtWidgets import QApplication
 from windows.game_window import GameWindow
-from core.fake_core_for_test import MockCoreAPI   #REAL_IMPORT
+import go_engine as go
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    
-    #REAL_CORE_INSTANTIATE_HERE
-    core_api = MockCoreAPI(19)
+    core_api = go.Game(19)
     
     window = GameWindow(navigation=None, board_size=19, core_api=core_api)
     window.show()
